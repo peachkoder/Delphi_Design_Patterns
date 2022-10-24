00a7978903f61bb85cc9c403020c1b2a1186c6d1
peachkoder <peachkoder@gmail.org>
2022-10-25 00:03:57 +0100

    Factory

--- design_patterns.dpr
+++ design_patterns.dpr
@@ -6,13 +6,20 @@ program design_patterns;
 
 uses
   System.SysUtils,
-  Builder in 'Builder.pas';
+  Builder in 'Builder.pas',
+  factory in 'factory.pas';
 
 begin
 
+  // Creational: Builder
   var User := TUserDirector.Construct(TUserBuilder.Create());
-
   writeln(User.ToString);
+
+  // Creational: Factory
+  var factory := TFactory.Create;
+  writeln(factory.CreateProduct('tablet').GetPrice);
+
+
 
 
   try
---
+++ factory.pas
@@ -0,0 +1,86 @@
+{*******************************************************}
+{                                                       }
+{              DESIGN PATTERNS IN DELPHI                }
+{                                                       }
+{                CATEGORY : CREATIONAL                  }
+{                                                       }
+{                   TYPE  : FACTORY                     }
+{                                                       }
+{                                                       }
+{*******************************************************}
+
+unit factory;
+
+interface
+
+type
+
+IProduct = interface
+  function GetPrice: Currency;
+end;
+
+TCellular = class(TInterfacedObject, IProduct)
+  function GetPrice: Currency;
+end;
+
+TTablet = class(TInterfacedObject, IProduct)
+  function GetPrice: Currency;
+end;
+
+TCamera = class(TInterfacedObject, IProduct)
+  function GetPrice: Currency;
+end;
+
+IFactory = interface
+  function CreateProduct(Name: String): IProduct;
+end;
+
+TFactory = class(TInterfacedObject, IFactory)
+  function CreateProduct(Name: String): IProduct;
+end;
+
+
+implementation
+
+
+
+{ TFactory }
+
+
+
+{ TFactory }
+
+function TFactory.CreateProduct(Name: String): IProduct;
+begin
+  if Name = 'cellular' then
+    Exit(TCellular.Create)
+  else if Name = 'tablet' then
+    Exit(TTablet.Create)
+  else if Name = 'camera' then
+    Exit(TCamera.Create)
+  else
+    Exit(TCellular.Create);
+end;
+
+{ TCellular }
+
+function TCellular.GetPrice: Currency;
+begin
+   result := 1000.00;
+end;
+
+{ TTablet }
+
+function TTablet.GetPrice: Currency;
+begin
+   result := 550.00;
+end;
+
+{ TCamera }
+
+function TCamera.GetPrice: Currency;
+begin
+   result := 800.00;
+end;
+
+end.
